8638d129969c3e014459a135d21eadc650b58383
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-10-05 21:32:35 +0200

    initial

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const { createLambda, download, getNodeVersion, getSpawnOptions, FileBlob, getWritableDirectory  } = require('@vercel/build-utils')
+const path = require('path')
+exports.config = {
+    maxLambdaSize: '10mb'
+  }
+
+module.exports = {
+    
+    /**     * 
+     * @param {import('@vercel/build-utils/dist').BuildOptions} options 
+     */
+    async build(options) {
+        
+
+        const lambda = createLambda({
+            runtime: 'nodejs12.x',
+            handler: 'index.main',
+            files: {
+              'index.js': new FileBlob({ data: 'exports.main = () => {}' }),
+            },
+        });
+        return {
+          output: lambda,
+          routes: [
+            // If your Runtime needs to define additional routing, define it here…
+          ],
+        // return build result
+    }
+}
